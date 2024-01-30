f59d330228eae6e61f52cbcbc6f64fa1576714a7
yunanchang <asdqscesz70731@yahoo.com.tw>
2024-01-30 16:38:29 +0800

    0

---
+++ front/result.php
@@ -0,0 +1,15 @@
+<?php
+
+$que=$Que->find($_GET['id']);
+
+?>
+<fieldset>
+    <legend>目前位置:首頁>問卷調查><?=$que['text'];?></legend>
+    <h3><?=$que['text']?></h3>
+  <?php
+    $opts=$Que->all(['subject_id'=>$_GET['id']]);
+    foreach($opts as $opt){
+        $total=($que['vote']);
+    }
+  ?>
+</fieldset>
